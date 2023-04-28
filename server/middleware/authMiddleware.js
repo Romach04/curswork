9c3f70ce3ff7e01eec235eb19211ef7d4f6a4eec
Roman <rmayorov@gmail.com>
2023-04-28 19:31:28 +0400

    back_end

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,8 +6,7 @@ module.exports = function (req, res, next) {
     }
 
     try {
-        const token = req.headers.authorization.split('')[1]
-
+        const token = req.headers.authorization.split(' ')[1]
         if (!token) {
             return res.status(401).json({message: "Не авторизованый пользователь"})
         }
@@ -16,6 +15,6 @@ module.exports = function (req, res, next) {
         next()
         
     } catch (e) {
-        res.status(401).json({message: "Не авторизованый пользователь"})
+        res.status(401).json({message: "Не авторизованый пользователь e"})
     }
 }
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router()
+const authMiddleWare = require("../middleware/authMiddleware")
 const userController = require('../controllers/userController')
 
 
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth', authMiddleWare ,userController.check)
 
 module.exports = router
